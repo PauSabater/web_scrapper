b677f97f77233b79f86a1083b74bd11b957f641b
PauSabater <pau.sabater.vilar@gmail.com>
2024-05-22 13:10:00 +0200

    corrections general

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import verbs from './top-german-verbs.json' assert { type: "json" }
 import fs from "fs"
 
-// console.log(verbs)
 const data = verbs.verbs.map((verb) => {
     return verb.Infinitiv
 })
-
-console.log(data)
 
 fs.writeFileSync('verbs.js', JSON.stringify(data, null, 2))
