2c3e65ec079f1041b2246ea6c6449e585205f487
PauSabater <pau.sabater.vilar@gmail.com>
2024-07-02 10:20:28 +0200

    add changes on scrapping script

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,27 @@
 //@ts-ignore
 import { PlaywrightCrawler } from 'crawlee'
-import { verbsList } from '../list.js'
+import { verbsList } from './missingverbs.js'
 
 interface IVerbConjugation {
     isNull?: boolean,
     person?: string,
     conjugation?: string,
     conjugationHTML?: string
+    test?: string,
+    numCols?: number
+    text1?: string
+    text2?: string
+    text3?: string
+    text4?: string
+    text5?: string
 }
 
 // Initiate PlaywrightCrawler class
 const crawler = new PlaywrightCrawler({
     // Only need one page, the corresponding from the page
-    maxRequestsPerCrawl: 9,
+    maxRequestsPerCrawl: 60,
+    maxConcurrency: 1,
+    // maxRequestsPerMinute: 10,
 
     // Use the requestHandler to process each of the crawled pages.
     async requestHandler({ request, page, enqueueLinks, log, pushData }) {
@@ -26,7 +35,7 @@ const crawler = new PlaywrightCrawler({
 
             // Cleans and checks string for verbs data
             const cleanStr = (str: string)=> {
-                return str ? str.replace("⁵", "&#42;").trim() : ''
+                return str ? str.replace("⁵", "&#42;").replace("³", "¹").trim() : ''
             }
 
             // Gets the properties from a verb html element:
@@ -38,6 +47,8 @@ const crawler = new PlaywrightCrawler({
                 const person = numCols > 1 ? elsCols[!isImperative ? 0 : 1].innerHTML.trim() || '' : ''
                 let conjugation: string
                 let conjugationHTML: string
+                let preconjugation: string
+                let preconjugationHTML: string
 
                 if (numCols === 1) {
                     conjugation = cleanStr(elsCols[0].innerText)
@@ -50,8 +61,17 @@ const crawler = new PlaywrightCrawler({
                 }
 
                 if (numCols === 3) {
-                    conjugation = `${cleanStr(elsCols[1].innerText)} ${cleanStr(elsCols[2].innerText)}` || defaultStr,
-                    conjugationHTML = `${cleanStr(elsCols[1].innerHTML)} ${cleanStr(elsCols[2].innerHTML)}`
+                    conjugation = !isImperative
+                        ? `${cleanStr(elsCols[1].innerText)} ${cleanStr(elsCols[2].innerText)}` || defaultStr
+                        : cleanStr(elsCols[2].innerText),
+                    conjugationHTML = !isImperative
+                        ? `${cleanStr(elsCols[1].innerHTML)} ${cleanStr(elsCols[2].innerHTML)}`
+                        : cleanStr(elsCols[2].innerHTML)
+
+                    if (isImperative) {
+                        preconjugation = cleanStr(elsCols[0].innerText)
+                        preconjugationHTML = cleanStr(elsCols[0].innerHTML)
+                    }
                 }
 
                 if (numCols === 4) {
@@ -60,11 +80,29 @@ const crawler = new PlaywrightCrawler({
                 }
 
                 if (conjugation && conjugationHTML) return {
+                    ...preconjugation && {preconjugation: preconjugation},
+                    ...preconjugationHTML && {preconjugationHTML: preconjugationHTML},
                     ...person && {person: person},
                     conjugation: conjugation,
                     conjugationHTML: conjugationHTML
                 }
-                else return {isNull: true}
+
+                // case for reflexive
+                if (numCols === 5) return {
+                    ...person && {person: person},
+                    conjugation: `${cleanStr(elsCols[1].innerText)} ${cleanStr(elsCols[2].innerText)} ${cleanStr(elsCols[3].innerText)} ${cleanStr(elsCols[4].innerText)}`,
+                    conjugationHTML: `${cleanStr(elsCols[1].innerHTML)} ${cleanStr(elsCols[2].innerHTML)} ${cleanStr(elsCols[3].innerHTML)}  ${cleanStr(elsCols[4].innerHTML)}`
+                }
+
+                // else return {
+                //     numCols: numCols,
+                //     text1: elsCols[0].innerText,
+                //     text2: elsCols[1].innerText,
+                //     text3: elsCols[2].innerText,
+                //     text4: elsCols[3].innerText,
+                //     text5: elsCols[4].innerText,
+                //     isNull: true
+                // }
             }
 
             // Query page elementsinfinit
@@ -80,20 +118,24 @@ const crawler = new PlaywrightCrawler({
             // Query verb information
             const verbLevel = elSectionMain.querySelector(".bZrt")
             const elVerbInfo = elSectionMain.querySelector(".rInf") as HTMLElement
-            const verbInfinitive = elSectionMain.querySelectorAll("#grundform b")
-            const stemFormation = elSectionMain.querySelector("#stammformen") as HTMLElement
+            const verbInfinitive = elSectionMain.querySelector("#grundform")
+            verbInfinitive.lastElementChild.remove()
 
+            const stemFormation = elSectionMain.querySelector("#stammformen") as HTMLElement
             const elVerbTranslations = article.querySelector("#Uebersetzungen").nextElementSibling as HTMLElement
             const verbEnTranslation = elVerbTranslations.querySelector('[lang="en"]') as HTMLElement
             const verbEsTranslation = elVerbTranslations.querySelector('[lang="es"]') as HTMLElement
             const verbFrTranslation = elVerbTranslations.querySelector('[lang="fr"]') as HTMLElement
             const verbDeMeaning = article.querySelector('.rNt i') as HTMLElement
 
-            let verbHTML = ''
-            Array.from(verbInfinitive).forEach(element => {verbHTML = verbHTML + element.innerHTML})
+            let verbHTML = verbInfinitive.innerHTML
+            // Array.from(verbInfinitive).forEach(element => {verbHTML = verbHTML + element.innerHTML})
 
             // Remove the link from stem
             stemFormation.lastElementChild?.remove()
+
+            const isVerbSeparable = elVerbInfo.innerText.includes(' trennbar') ? true : false
+            const isReflexive = elVerbInfo.innerText.includes(' reflexiv') ? true : false
 
             // Add generic verb information to object
             let completeData: any = {
@@ -102,7 +144,9 @@ const crawler = new PlaywrightCrawler({
                     verbHTML: verbHTML,
                     stemFormationHTML: stemFormation?.innerHTML || '',
                     isIrregular: elVerbInfo.innerText.includes('unregelmäßig') ? true : false,
-                    isSeparable: elVerbInfo.innerText.includes('trennbar') ? true : false,
+                    isSeparable: isVerbSeparable,
+                    ...isReflexive && { reflexive: true },
+                    ...elVerbInfo.innerText.includes(' untrennbar') && { prefixed:  true },
                     auxiliary: elVerbInfo.innerText.includes('haben') ? 'haben' : 'sein',
 
                     translations: {
@@ -121,11 +165,16 @@ const crawler = new PlaywrightCrawler({
             }
 
             for (const [index, elSection] of elsSections.entries()) {
-                let scrapedFormData: {elSectionMain?: any, nonExistent?: boolean, tense?: string, conjugations?: any}[] = []
+                let scrapedFormData: {
+                    elSectionMain?: any,
+                    nonExistent?: boolean,
+                    tense?: string,
+                    conjugations?: any
+                } = {}
 
                 // Case if form does not exist, ie with Conjunctive on verb "sein"
                 if (elSection === null) {
-                    {scrapedFormData.push({nonExistent: true})}
+                    {scrapedFormData.nonExistent = true}
                     continue
                 }
 
@@ -141,10 +190,15 @@ const crawler = new PlaywrightCrawler({
                     const tense = index === 2 ? "Imperative" : (elTable.querySelector('h3') as HTMLElement).innerText
 
                     if (tense && objConjugations) {
-                        scrapedFormData.push({
-                            tense: tense,
-                            conjugations: objConjugations
-                        })
+                        const tenseEnding = tense.slice(-2)
+                        const tenseLowerCase = tense.slice(0, -2).toLowerCase()
+                        const tenseKey = (tenseLowerCase + tenseEnding).replaceAll(' ', '_').replaceAll('.', '')
+
+                        {
+
+                        }
+                        scrapedFormData[tenseKey] = {}
+                        scrapedFormData[tenseKey].conjugations = objConjugations
                     }
                 }
 
@@ -165,9 +219,9 @@ const crawler = new PlaywrightCrawler({
 
         // Save results as JSON to ./storage/datasets/default
         await pushData({
-                _id: request.loadedUrl.split("konjugation/")[1].replace('.htm', '').replaceAll('u3', 'ü').replaceAll('o3', 'ö') || '',
+                _id: request.loadedUrl.split("konjugation/")[1].replace('.htm', '').replaceAll('u3', 'ü').replaceAll('o3', 'ö').replaceAll('s5', 'ß').replaceAll('a3', 'ä') || '',
                 url: request.loadedUrl,
-                verb: request.loadedUrl.split("konjugation/")[1].replace('.htm', '').replaceAll('u3', 'ü').replaceAll('o3', 'ö') || '',
+                verb: request.loadedUrl.split("konjugation/")[1].replace('.htm', '').replaceAll('u3', 'ü').replaceAll('o3', 'ö').replaceAll('s5', 'ß').replaceAll('a3', 'ä') || '',
                 data: dataVerb
             }
         )
